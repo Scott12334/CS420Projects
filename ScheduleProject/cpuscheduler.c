027cb74126ce8994b062adc4936bdfd033a5f931
Scott <scottnicholson113@gmail.com>
2024-11-15 12:02:41 -0500

    SJF

--- ScheduleProject/cpuscheduler.c
+++ ScheduleProject/cpuscheduler.c
@@ -15,7 +15,8 @@ typedef struct Link{
 }Link;
 void readInQueue(Link ** end, FILE * inputFile);
 void RR(Link ** head, Link ** end,int timeQuantum, FILE * outputFile);
-void SJF();
+void SJF(Link ** head, Link ** end, int jobCount, FILE * outputFile);
+Link * sortedProcesses(Link ** queueHead, int numProcesses);
 void PR_noPREMP();
 void PR_withPREMP();
 int main(int argc, char * argv[]){
@@ -27,6 +28,10 @@ int main(int argc, char * argv[]){
 	//13 is the max size of the schedule type +1 for terminating 0
 	char * scheduleType = (char *)calloc(sizeof(char),13); 
 	fgets(scheduleType,13,inputFile);
+	char * processCount = (char *)calloc(sizeof(char),5); 
+	fgets(processCount,5,inputFile);
+	int numProcess = atoi(processCount);
+
 	Link * head = (Link *)malloc(sizeof(Link));
 	Link * end = (Link *)malloc(sizeof(Link));
 	head->next = end;
@@ -39,6 +44,9 @@ int main(int argc, char * argv[]){
 		readInQueue(&end, inputFile);
 		RR(&head,&end,number,outputFile);
 	}else if(strncmp("SJF",scheduleType,3) == 0){
+		fwrite(scheduleType,sizeof(char),strlen(scheduleType),outputFile);
+		readInQueue(&end, inputFile);
+		SJF(&head, &end, numProcess,outputFile);
 		printf("SJF");
 	}else if(strncmp("PR_noPREMP",scheduleType,10) == 0){
 		printf("PR_noPREMP");
@@ -54,16 +62,13 @@ void enqueue(Link ** end, Job * newJob){
 	(*end)->prev = newLink;
 	newLink->next = (*end);
 }
-Job * dequeue(Link ** head){
+Link * dequeue(Link ** head){
 	Link *nodeToRemove = (*head)->next;
-    Job *nextOut = nodeToRemove->value;
 
     (*head)->next = nodeToRemove->next;
     nodeToRemove->next->prev = (*head);
 
-    free(nodeToRemove);  
-
-	return nextOut;
+	return nodeToRemove;
 }
 void readInQueue(Link ** end, FILE * inputFile){
 	char * currentLine = (char *)calloc(sizeof(char),15); 
@@ -85,7 +90,7 @@ void RR(Link ** head, Link ** end,int timeQuantum, FILE * outputFile){
 		sprintf(result,"%d %d\n",time,(*head)->next->value->number);
 		fwrite(result,sizeof(char),strlen(result),outputFile);
 		//Pop off first queue object
-		Job * nextJob = dequeue(head);
+		Job * nextJob = (dequeue(head))->value;
 		if(nextJob->cpuBurst < timeQuantum){time += nextJob->cpuBurst;}
 		else{time += timeQuantum;}
 		//Subtract time quantum from CPU burst
@@ -94,6 +99,44 @@ void RR(Link ** head, Link ** end,int timeQuantum, FILE * outputFile){
 		if(nextJob->cpuBurst > 0){enqueue(end,nextJob);}
 	}
 }
-void SJF(){}
+void SJF(Link ** head, Link ** end, int jobCount, FILE * outputFile){
+	int time = 0;
+	//Create sorted linked list of processes
+	Link * sortedHead = sortedProcesses(head, jobCount);
+	sortedHead = sortedHead->next;
+	while(sortedHead->next != NULL){
+		printf("%d", sortedHead->value->cpuBurst);
+		sortedHead = sortedHead->next;
+	}
+	//Move through array, if the process has arrived by the time, run it, if not move to next shortest, add to time
+	//If reach end and nothing has run, add one to time 
+}
 void PR_noPREMP(){}
 void PR_withPREMP(){}
+Link * sortedProcesses(Link ** queueHead, int numProcesses){
+	Link * sortedHead = (Link *)malloc(sizeof(Link));
+	Link * storedHead = sortedHead;
+	sortedHead->next = dequeue(queueHead);
+	sortedHead->next->prev = sortedHead;
+	for(int i = 1; i < numProcesses; i++){
+		Link * nextLink = dequeue(queueHead);
+		sortedHead = storedHead->next;
+		//Loop through the new list until you either reach the end or find a spot where the cpu burst is less then the next one
+		while(sortedHead->next != NULL){
+			if(nextLink->value->cpuBurst < sortedHead->value->cpuBurst){
+				sortedHead->prev->next = nextLink;
+				nextLink->prev = sortedHead->prev;
+				sortedHead->prev = nextLink;
+				nextLink->next = sortedHead;
+				break;
+			}
+			sortedHead = sortedHead->next;
+			if(sortedHead->next == NULL){
+				sortedHead->next = nextLink;
+				nextLink->prev = sortedHead;
+				break;
+			}
+		}
+	}
+	return sortedHead;
+}
